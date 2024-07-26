be64d789e279d748a2cfc912a464bc8430cbde40
arthur <arthur@darkstar.example.net>
2024-07-27 00:41:28 +1200

    Keep track of currently selected audio device in .asoundrc

--- anal-generateAudioMenu.pl
+++ anal-generateAudioMenu.pl
@@ -1,20 +1,48 @@
 #!/usr/bin/perl
 
+$HOME = $ENV{'HOME'};
+
+$selectedNum = '';
+$selectedName = '';
+
+$asoundrc = `cat "$HOME/.asoundrc"`;
+if ($asoundrc =~ m/^#ANaL hw:(\d+)/) {
+    $selectedNum = $1;
+}
+
 $output = `cat /proc/asound/cards`;
 @lines = split "\n", $output;
 $count = 0;
 foreach $line (@lines) {
     if ($line =~ m/^\s*(\d+)[^:]+: (.+)/) {
+        $num = $1;
         $id = "hw:$1";
         $name = $2;
+        if ($selectedNum eq $num) {
+            $selectedName = $name;
+            $name = "* $name";
+        } else {
+            $name = "  $name";
+        }
         print <<EOF;
 =id=$id
 =name=$name
 =stringFormat=#{name} (#{id})
-=messageForClick=NSArray|addObject:'anal-openALSAPanel.sh'|addObject:id|addObject:name|runCommandInBackground
+=messageForClick=NSArray|addObject:'anal-selectAudioDevice.pl'|addObject:'$num'|runCommandInBackground
 ==
 EOF
         $count++;
     }
+}
+
+if ($selectedNum =~ m/^\d+$/) {
+    print <<EOF;
+==
+=id=hw:$selectedNum
+=name=$selectedName
+=displayName=Open Mixer...
+=messageForClick=NSArray|addObject:'anal'|addObject:'ALSAPanel::'|addObject:id|addObject:name|runCommandInBackground
+==
+EOF
 }
 
