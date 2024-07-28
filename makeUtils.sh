05f16948e27c606b270479c6ceede49576844daf
arthur <arthur@darkstar.example.net>
2024-07-29 02:30:50 +1200

    Renamed ALSA

--- makeUtils.sh
+++ makeUtils.sh
@@ -3,12 +3,12 @@
 set -x
 
 gcc -o anal-alsa-generatePanel anal-alsa-generatePanel.c -lasound
-gcc -o anal-printALSAUpdates anal-printALSAUpdates.c -lasound
-gcc -o anal-printALSAStatus anal-printALSAStatus.c -lasound
-gcc -o anal-printALSAFirstElement anal-printALSAFirstElement.c -lasound
-gcc -o anal-setALSAMute: anal-setALSAMute:.c -lasound
-gcc -o anal-setALSAVolume anal-setALSAVolume.c -lasound
-gcc -o anal-setALSAValues anal-setALSAValues.c -lasound
+gcc -o anal-alsa-printFirstElement anal-alsa-printFirstElement.c -lasound
+gcc -o anal-alsa-printUpdates anal-alsa-printUpdates.c -lasound
+gcc -o anal-alsa-printStatus anal-alsa-printStatus.c -lasound
+gcc -o anal-alsa-setMute: anal-alsa-setMute:.c -lasound
+gcc -o anal-alsa-setVolume anal-alsa-setVolume.c -lasound
+gcc -o anal-alsa-setValues anal-alsa-setValues.c -lasound
 gcc -o anal-monitorBlockDevices anal-monitorBlockDevices.c
 gcc -o anal-monitorNetworkInterfaces anal-monitorNetworkInterfaces.c
 gcc -o anal-monitorMonitors anal-monitorMonitors.c
