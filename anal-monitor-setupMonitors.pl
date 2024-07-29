75e25fb41213483bda1ee4383d302e4f4deb5e2b
arthur <arthur@darkstar.example.net>
2024-07-30 01:41:54 +1200

    Renamed

--- anal-monitor-setupMonitors.pl
+++ anal-monitor-setupMonitors.pl
@@ -4,11 +4,11 @@ $baseDir = `anal configDir`;
 chomp $baseDir;
 chdir $baseDir;
 
-system('cat Temp/monitors.txt | anal-generateSetupMonitorsScriptFromMonitorsFile.pl | sh');
+system('cat Temp/monitors.txt | anal-monitor-generateScriptFromFile.pl | sh');
 if (not -d 'Temp') {
     system('mkdir', 'Temp');
     system('chmod', '1777', 'Temp');
 }
 
-system('anal-listMonitors.pl >Temp/listMonitors.txt');
+system('anal-monitor-listMonitors.pl >Temp/listMonitors.txt');
 
