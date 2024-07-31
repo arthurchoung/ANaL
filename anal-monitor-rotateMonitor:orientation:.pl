3c7e174fbf3822640070f039371f9c2d16d56194
arthur <arthur@darkstar.example.net>
2024-08-01 01:17:42 +1200

    Change directory to location of script

--- anal-monitor-rotateMonitor:orientation:.pl
+++ anal-monitor-rotateMonitor:orientation:.pl
@@ -6,9 +6,11 @@ if (not $matchName or not $orientation) {
     die('specify matchName and orientation');
 }
 
-$baseDir = `anal configDir`;
-chomp $baseDir;
-chdir $baseDir;
+$baseDir = __FILE__;
+$baseDir =~ s/[^\/]*$//g;
+if ($baseDir) {
+    chdir $baseDir;
+}
 
 system("cat Temp/listMonitors.txt | anal-monitor-modifyToRotateMonitor:orientation:.pl $matchName $orientation >Temp/monitors.txt");
 
